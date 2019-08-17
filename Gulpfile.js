6256adda21d1b215e2f70c84c8dfe7684d874a36
Luis Colon <luiscolon62@gmail.com>
2019-08-17 17:30:05 -0400

    Fixed security vulnerabilities

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,10 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const uglifyjs = require('gulp-uglifyjs');
+const uglify = require('gulp-uglify');
 
 gulp.task('default', () => {
-  return gulp.src('src/**/*.js')
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(uglifyjs())
+  return gulp.src('src/index.js')
+    .pipe(babel({presets: ['@babel/preset-env']}))
+    .pipe(uglify())
     .pipe(gulp.dest('dist'))
 });
--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,26 @@
-"use strict";function numbersOnly(e){var t=e.which;return!e.shiftKey&&(8===t||37===t||39===t||9===t||t>=48&&t<=58||t>=96&&t<=105)||(e.preventDefault(),!1)}function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=numbersOnly;var _index=require("./index"),_index2=_interopRequireDefault(_index);describe("numbersOnly()",function(){it("does not allow the user to hit the shift key",function(){var e={shiftKey:!0,which:42,preventDefault:jest.fn(function(){})};expect((0,_index2.default)(e)).toEqual(!1)}),it("does not allow the user to enter in anything but numbers",function(){var e={shiftKey:!1,which:200,preventDefault:jest.fn(function(){})};expect((0,_index2.default)(e)).toEqual(!1)}),it("allows the user to use the delete key",function(){var e={shiftKey:!1,which:8,preventDefault:jest.fn(function(){})};expect((0,_index2.default)(e)).toEqual(!0)}),it("allows the user to use the tab key",function(){var e={shiftKey:!1,which:9,preventDefault:jest.fn(function(){})};expect((0,_index2.default)(e)).toEqual(!0)}),it("allows the user to use the left arrow key",function(){var e={shiftKey:!1,which:37,preventDefault:jest.fn(function(){})};expect((0,_index2.default)(e)).toEqual(!0)}),it("allows the user to use the right arrow key",function(){var e={shiftKey:!1,which:39,preventDefault:jest.fn(function(){})};expect((0,_index2.default)(e)).toEqual(!0)}),it("allows the user to enter a keyboard number",function(){var e={shiftKey:!1,which:50,preventDefault:jest.fn(function(){})};expect((0,_index2.default)(e)).toEqual(!0)}),it("allows the user to enter a numpad number",function(){var e={shiftKey:!1,which:100,preventDefault:jest.fn(function(){})};expect((0,_index2.default)(e)).toEqual(!0)})});
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+/**
+ * numbersOnly()
+ * JS event for only allowing numbers and a few special characters
+ * in a form input
+ */
+function numbersOnly(e) {
+  const charCode = e.which; // We only want to allow certain special characters and numbers
+  // for our currency input (0-9, tab, delete, left and right arrow)
+
+  if (!e.shiftKey && (charCode === 8 || charCode === 37 || charCode === 39 || charCode === 9 || charCode >= 48 && charCode <= 58 || charCode >= 96 && charCode <= 105)) {
+    return true;
+  }
+
+  e.preventDefault();
+  return false;
+}
+
+var _default = numbersOnly;
+exports.default = _default;
